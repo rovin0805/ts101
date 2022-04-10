678eedd7390a9e7a96b7b6e021444cbe715cfb35
Choi Eun Jeong <qhdrn67@naver.com>
2022-04-10 13:07:17 +0900

    added some black fns

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJs from 'crypto-js';
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calBlockHash = (
+    index: number,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ): string =>
+    CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -21,7 +32,12 @@ class Block {
 
 const genesisBlock: Block = new Block(0, 'hashStr', '', 'dataStr', 1234);
 
-let blockchain: [Block] = [genesisBlock];
-console.log(blockchain);
+let blockchain: Block[] = [genesisBlock];
+
+const getBlackChain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
