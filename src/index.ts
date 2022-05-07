2cdae538a2262a7fb5c0356909a85b54310515d8
Choi Eun Jeong <qhdrn67@naver.com>
2022-05-07 22:58:09 +0900

    Blockchain

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,38 @@ class Block implements IBlock {
     return crypto.createHash('sha256').update(toHash).digest('hex');
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) {
+      return '';
+    } else {
+      const lastBlock = this.blocks[this.blocks.length - 1];
+      return lastBlock.hash;
+    }
+  }
+
+  public addBlock(data: string) {
+    const prevHash = this.getPrevHash();
+    const height = this.blocks.length + 1;
+    const newBlock = new Block(prevHash, height, data);
+    this.blocks.push(newBlock);
+  }
+
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock('1st one');
+blockchain.addBlock('2nd one');
+blockchain.addBlock('3rd one');
+
+console.log(blockchain.getBlocks());
